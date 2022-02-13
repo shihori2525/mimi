43cde00dcfabb942681e1e19e8dc69e96f4d24dc
shihori2525 <rirakkuma.250@gmail.com>
2022-02-13 08:57:12 +0000

    [Add]post comment Ajax

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,7 +1,27 @@
 class Public::PostCommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @comment = current_member.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      render :post_comments
+    else
+      render 'posts/show'
+    end
   end
 
   def destroy
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+
+    #renderしたときに@postのデータがないため記述
+    @post = Post.find(params[:post_id])
+    render :post_comments
   end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+
 end
