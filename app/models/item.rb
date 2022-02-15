19a03e7569bf46defb3889dd1ef36e5061cc4c2c
shihori2525 <rirakkuma.250@gmail.com>
2022-02-15 14:00:22 +0000

    [Add]post item posts

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_many :posts,dependent: :destroy
+  has_many :favorited_members,through: :item_favorites,source: :member
 
   has_many :item_favorites,dependent: :destroy
   def item_favorited_by?(member)
@@ -11,8 +12,21 @@ class Item < ApplicationRecord
 
   attachment :item_image
 
+  #検索機能
   def self.search_for(word)
     @items = Item.where(['name LIKE ?', "%#{word}%"])
+  end
+
+  #ソート機能
+  def self.sort(selection)
+    case selection
+    when 'new'
+      return includes(:post).order("posts.created_at DESC")
+    when 'old'
+      return includes(:post).order("posts.created_at ASC")
+    when 'likes'
+      return includes(:favorited_members).sort {|a,b| b.favorited_members.size <=> a.favorited_members.size}
+    end
   end
 
 
