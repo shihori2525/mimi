042a8534c91d3db974c5c7354308db2b63c1f6f3
shihori2525 <rirakkuma.250@gmail.com>
2022-02-22 09:26:26 +0000

    [Add]test file

--- db/schema.rb
+++ db/schema.rb
@@ -132,7 +132,6 @@ ActiveRecord::Schema.define(version: 2022_02_14_144307) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
 end
