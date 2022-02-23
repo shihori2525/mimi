ebd92203c69a1c0e17ebede6f89ed7eb96f6e3ff
shihori2525 <rirakkuma.250@gmail.com>
2022-02-23 02:45:19 +0000

    [Fix]refile secret key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -7,4 +7,4 @@
 #   )
 # end
 
-Refile.secret_key = '3ae7510c009144830fd4c6ebe9c81f347efc58983d2653f20692cb536ed9e23314a900949e4648a6a1bba259229ba89b41603d3e02a839b5affc0180c8876ce8'
+Refile.secret_key = ENV['REFILE_SECRET_KEY']
